8fc39750366a6faa79cf9b1c84084d599732d5c3
Adnan Tayib <adnantayib8@gmail.com>
2024-09-12 18:12:55 -0400

    Implemented the basic HTTP methods

--- main.py
+++ main.py
@@ -29,9 +29,75 @@ class Blog(db.Model):
             }
 
 
+#Post a new blog
+@app.route("/blog", methods=["POST"])
+def post_blog():
+
+    data = request.get_json()
+
+    #Create new blog
+    blog = Blog(
+        title = data["title"],
+        content = data["content"],
+        tag = data.get("tag", "")
+    )
+
+    #add the blog to the db
+    db.session.add(blog)
+    db.session.commit()
+
+    return jsonify({"message": "Blog posted successfully", "Blog": blog.to_dict()}), 201
 
 
+#Return all blogs
+@app.route("/blogs", methods=["GET"])
+def get_blogs():
 
+    #get all blogs
+    blogs = Blog.query.all()
+
+    return jsonify([blog.to_dict() for blog in blogs]), 200
+
+#Return a single blog
+@app.route("/blog/<int:id>", methods=["GET"])
+def get_blog(id):
+
+    blog = Blog.query.get(id)
+
+    if blog:
+        return jsonify({"blog": blog.to_dict()}), 200
+    else:
+        return jsonify({"error": "blog post not found."}), 404
+
+#Update a single blog
+@app.route("/blog/<int:id>", methods=["PUT"])
+def update_blog(id):
+
+    blog = Blog.query.get(id)
+    data = request.get_json()
+
+    if blog:
+        blog.title = data.get("title", blog.title)
+        blog.content = data.get("content", blog.content)
+        blog.tag = data.get("tag", blog.tag)
+
+        db.session.commit()
+        return jsonify({"message": "blog post updated successfully", "blog": blog.to_dict()}), 200
+    
+    return jsonify({"error": "blog post not found"}), 404
+
+#Delete a blog post
+@app.route("/blog/<int:id>", methods=["DELETE"])
+def delete_blog(id):
+
+    blog = Blog.query.get(id)
+
+    if blog:
+        db.session.delete(blog)
+        db.session.commit()
+        return jsonify({"message": "blog post deleted successfully"}), 200
+    
+    return jsonify({"error": "blog post not found."}), 404
 
 
 
