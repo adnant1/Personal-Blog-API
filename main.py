7010df395708587125882829c4369bff83f9eb41
Adnan Tayib <adnantayib8@gmail.com>
2024-09-14 00:15:09 -0400

    Connected to user database and implemented user routes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 #Create flask instance
 app = Flask(__name__)
+
+#Configure secret key
+#Fix later before making repo public
+app.config["SECRET_KEY"] = 'flint346297'
 
 #Configure SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlconnector://root:Flint346297@localhost/blog_db'
@@ -28,6 +34,105 @@ class Blog(db.Model):
             "tag": self.tag
             }
 
+#User model
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    #When the token is decoded this is the id that will show up, will not match the actual id from the table
+    public_id = db.Column(db.String(50), unique = True)
+    name = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    admin = db.Column(db.Boolean, default = False)
+
+    #to dict method
+    def to_dict(self):
+        return {
+            "public_id": self.public_id,
+            "name": self.name,
+            "password": self.password,
+            "admin": self.admin
+        }
+
+
+#User Admin routes
+
+#Get all users
+@app.route('/users', methods=["GET"])
+def get_all_users():
+
+    users = User.query.all()
+
+    return jsonify([user.to_dict() for user in users]), 200
+
+
+#Return a single user
+@app.route('/user/<public_id>', methods=["GET"])
+def get_single_user(public_id):
+
+    #Use filter by when querying the data based off anything but the primary key
+    #Returns a query object, so use .first() to get the first user returned
+    user = User.query.filter_by(public_id = public_id).first()
+
+    if user:
+        return jsonify({"user": user.to_dict()}), 200
+    
+    return jsonify({"error": "user not found"}), 404
+
+
+#Create a user
+@app.route('/user', methods=["POST"])
+def create_user():
+
+    data = request.get_json()
+
+    #Hash the password, this method is the default method
+    hashed_password = generate_password_hash(data['password'], method="pbkdf2:sha256")
+
+    #Create new user
+    #UUID4 creates a UUID based on random numbers
+    new_user = User(
+        public_id = str(uuid.uuid4()),
+        name = data["name"],
+        password = hashed_password
+    )
+
+    #add the user to the db
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "new user created successfully"}), 201
+
+
+#Promote a user to admin
+@app.route('/user/<public_id>', methods=["PUT"])
+def promote_user(public_id):
+
+    user = User.query.filter_by(public_id = public_id).first()
+
+    if user:
+        user.admin = True
+        db.session.commit()
+        return jsonify({"message": "user promoted successfully"}), 200
+
+    return jsonify({"error": "user not found"}), 404
+
+
+#Delete a user
+@app.route('/user/<public_id>', methods=["DELETE"])
+def delete_user(public_id):
+
+    user = User.query.filter_by(public_id = public_id).first()
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "user deleted successfully"}), 200
+
+    return jsonify({"error": "user not found"}), 404
+
+
+#Blog routes
 
 #Post a new blog
 @app.route("/blog", methods=["POST"])
