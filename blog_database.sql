ce6e83c2467b5e39b2324a26b74922d7766c6c95
Adnan Tayib <adnantayib8@gmail.com>
2024-09-14 00:09:19 -0400

    created users table

--- blog_database.sql
+++ blog_database.sql
@@ -7,4 +7,13 @@ id int auto_increment PRIMARY KEY,
 title VARCHAR(50) NOT NULL,
 content TEXT NOT NULL,
 tag VARCHAR(50)
+);
+
+
+CREATE TABLE users (
+id int auto_increment PRIMARY KEY,
+public_id VARCHAR(50) unique,
+name VARCHAR(50) NOT NULL,
+password VARCHAR(255) NOT NULL,
+admin BOOLEAN DEFAULT FALSE
 )
